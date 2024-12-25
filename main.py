f35696663e1c46aaf7edc568a307c55cbff60648
Yuya Ueno <icci.ueno.yuya@gmail.com>
2024-12-25 22:03:19 +0900

    見出しを選択し本文を生成する機能の実装

--- main.py
+++ main.py
@@ -14,14 +14,35 @@ API_KEY = os.environ["OPENAI_KEY"]
 client = OpenAI(api_key=API_KEY)
 
 #必要な情報
+#FIXME 検証のための初期値を削除
 TITLE = "初めての自作PC! 初心者必見のマザーボードの選び方を徹底解説"
 REFER_URL = ["https://www.sofmap.com/contents/?id=nw_ps_select&sid=mb"]
-TARGET = ["高校生"]
-KEYWORD = ["自作PC", "マザーボード"]
+TARGETS = ["カップル"]
+KEYWORDS = ["自作PC", "マザーボード"]
 
-titles = prompt.suggest_title_from_keyword(client, input("keywords? ").split(","))
+#print(prompt.generate_body(client, title="", heading=""))
+
+KEYWORDS = input("キーワードを入力して下さい: ").split(",")
+titles = prompt.suggest_titles(client, keywords=KEYWORDS, targets=TARGETS)
+
 for i in range(len(titles)):
   print(f"{i}: {titles[i]}")
+num = int(input("番号を入力してください: "))
+TITLE = titles[num]
 
-res = prompt.suggest_outlines(client, titles[int(input("idx? "))])
-print(res)
+seo = prompt.suggest_seo_keywords(client, title=TITLE)
+print(f"関連キーワード: {",".join(seo)}")
+
+res = prompt.suggest_outlines(client, title=TITLE, keywords=KEYWORDS, targets=TARGETS)
+for i in range(len(res)):
+  print(f"{i}: {res[i]}")
+idx = input("採用する見出しを順に入力してください: ").split(",")
+heads = [res[int(x)] for x in idx]
+
+print("記事を生成しています...")
+for head in heads:
+  subs = prompt.suggest_subheadings(client, heading=head)
+  con = prompt.generate_body(client, title=TITLE, 
+                             heading=head, subheading=subs)
+  print(con)
+  
--- prompt.py
+++ prompt.py
@@ -1,62 +1,106 @@
 from openai import OpenAI
 from pydantic import BaseModel
+from typing import overload
 
-class TitleSuggestion(BaseModel):
-  titles: list[str]
+LLM_MODEL = "gpt-4o-mini"
 
-#入力されたキーワードをもとにタイトルの候補を生成します
-def suggest_title_from_keyword(client: OpenAI, keywords: list[str]) -> list[str]:
-  #目次を生成
-  completion = client.beta.chat.completions.parse(
-    model="gpt-4o-mini",
-    #TODO システムプロンプトはファイルに書いたが楽かも
-    messages=[ #TODO プロンプトへの入力は関数でまとめる
-        {"role": "system", "content": f"入力されたSEOキーワードのリストに関連する、クリックされるブログのタイトルを提案してください。"},
-        {"role": "user", "content": " ".join(keywords)}
-    ],
-    response_format=TitleSuggestion
-  )
+# 単一の文字列配列のみを持つ構造
+class StringArray(BaseModel):
+  array: list[str]
 
-  res = completion.choices[0].message.parsed
-  if (res is None):
-    raise RuntimeError("can't parse to json")
-  return res.titles
+@overload
+def request[T : BaseModel](client: OpenAI, system_msg: str, user_msg: str, format: type[T]) -> T: ...
 
-class KeywordSuggestion(BaseModel):
-  keywords: list[str]
+@overload
+def request(client: OpenAI, system_msg: str, user_msg: str, format: None = None) -> str: ...
+
+# APIを通して文章を生成します。formatにクラスを渡すことで構造化された結果が得られます
+def request[T : BaseModel](client: OpenAI, system_msg: str, user_msg: str, format: type[T] | None = None) -> T | str:
+  #フォーマットが指定されなかった時は構造化を利用しない
+  if format is None:
+    completion = client.chat.completions.create(
+      model=LLM_MODEL,
+      messages=[
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": user_msg}
+      ],
+    )
+    res = completion.choices[0].message.content
+    if res is None:
+      raise RuntimeError("can't parse to json")
+    return res
+    
+  #構造化出力を利用する
+  else:
+    # プロンプトを問い合わせる
+    completion = client.beta.chat.completions.parse(
+      model=LLM_MODEL,
+      messages=[
+          {"role": "system", "content": system_msg},
+          {"role": "user", "content": user_msg}
+      ],
+      response_format=format
+    )
+    res = completion.choices[0].message.parsed
+    if res is None:
+      raise RuntimeError("can't parse to json")
+    return res
+  
+# 入力されたキーワードをもとにタイトルの候補を生成します
+#TODO プロンプトは変更しやすいようにtxtファイルに分離する
+def suggest_titles(client: OpenAI, *, keywords: list[str], targets: list[str]) -> list[str]:
+  system = "入力された要件に合うような、SEO最適化されたブログのタイトルを10個提案してください。"
+  user = f"""
+    キーワード: 「{", ".join(keywords)}」
+    ターゲット: 「{", ".join(targets)}」
+    """
+  res = request(client, system, user, StringArray)
+  return res.array
 
 #検索欄の候補に表示されるようなキーワード候補をタイトルから考案します
 #TODO 数の指定・追加で生成できるようにしたい
-def suggest_seo_keywords(client: OpenAI, title: str) -> list[str]:
-  #目次を生成
-  completion = client.beta.chat.completions.parse(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": f"入力されたタイトルに関連するSEOキーワードを10個特定してください。"},
-        {"role": "user", "content": title}
-    ],
-    response_format=KeywordSuggestion
-  )
+def suggest_seo_keywords(client: OpenAI, *, title: str) -> list[str]:
+  system = "入力されたタイトルに関連するSEOキーワードを10個特定してください。"
+  user = f"記事のタイトル: 「{title}」"
+  res = request(client, system, user, StringArray)
+  return res.array
 
-  res = completion.choices[0].message.parsed
-  if (res is None):
-    raise RuntimeError("can't parse to json")
-  return res.keywords
+def suggest_outlines(client: OpenAI, *, title: str, keywords: list[str], targets: list[str]) -> list[str]:
+  system = "入力された項目を参考にして、ブログ記事のアウトラインの例を10個考えてください。"
+  user = f"""
+    記事のタイトル: 「{title}」
+    キーワード: 「{", ".join(keywords)}」
+    ターゲット: 「{", ".join(targets)}」
+    """
+  res = request(client, system, user, StringArray)
+  return res.array
 
-#class OutlineSuggestion(BaseModel):
+def suggest_subheadings(client: OpenAI, *, heading: str) -> list[str]:
+  system = "入力された要件に合うような、記事の小見出しをいくつか考えてください。"
+  user = f"""
+    見出し: 「{heading}」
+  """
+  res = request(client, system, user, StringArray)
+  return res.array
 
+def generate_body(client: OpenAI, *, 
+                     title: str, heading: str, subheading: list[str]) -> str:
+  system = """
+    初心者を対象とした親しみやすく助けになる雰囲気で、SEOを意識した記事の本文を書いてください。
+    文章は指定されたアウトラインの内容のみ書いてください。
 
-def suggest_outlines(client: OpenAI, title: str) -> list[str]:
-  #目次を生成
-  completion = client.beta.chat.completions.parse(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": f"あなたはSEOに精通したテクニカルライターと仮定します。初心者を対象とした親しみやすく助けになる雰囲気で、入力された要件に合ったステップバイステップガイドの詳しいブログ記事のアウトラインを作成してください。"},
-        {"role": "user", "content": f"タイトル: {title}"}
-    ],
-  )
-
-  res = completion.choices[0].message.content
+    出力: 
+    - タグはhtmlのようなマークアップ形式で出力してください。
+    - 段落ごとに「p」タグで区切ってください。
+    - 「##」の代わりに「h2」タグを、「###」の代わりに「h3」タグを使用してください。
+    - 重要なキーワードは「strong」タグで囲ってください。
+    """
+  sh = "\n".join([f"### {x}" for x in subheading])
+  user = f"""
+    記事のタイトル: 「{title}」
+    アウトライン:
+    ## {heading}
+    {sh}
+    """
+  res = request(client, system, user)
   return res
-  
-
